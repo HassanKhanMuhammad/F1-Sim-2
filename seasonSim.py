24636e3035a6b04f2f2a0b2e6a8446d8980fc1ec
Hassan Khan <khanmuhammadhassan2008@gmail.com>
2023-07-16 14:44:02 +0500

    Trying to figure out how to make season sim work

--- seasonSim.py
+++ seasonSim.py
@@ -13,38 +13,64 @@ from raceSim import *
 
 #         numberOfRacesCompleted += 1
 
+pointsThisSeason = {
+    "pointsMV": 0,
+    "pointsSP": 0,
+    "pointsLH": 0,
+    "pointsGR": 0,
+    "pointsCL": 0,
+    "pointsCS": 0,
+    "pointsFA": 0,
+    "pointsLS": 0
+    # TODO: add the rest
+    # points are zero in the start of the season.
+    }
+
+
+POINTS_DISTRIBUTION = {
+    "P1": 25,
+    "P2": 18,
+    "P3": 15,
+    "P4": 12,
+    "P5": 10,
+    "P6": 8,
+    "P7": 6,
+    "P8": 4,
+    "P9": 2,
+    "P10": 1
+    }
+
+
+
 class seasonSimulation():
 
-    pointsThisSeason = {
-        "pointsMV": 0,
-        "pointsSP": 0,
-        "pointsLH": 0,
-        "pointsGR": 0,
-        "pointsCL": 0,
-        "pointsCS": 0,
-        "pointsFA": 0,
-        "pointsLS": 0
-        # TODO: add the rest
-        # points are zero in the start of the season.
-    }
 
-
-    POINTS_DISTRIBUTION = {
-        "P1": 25,
-        "P2": 18,
-        "P3": 15,
-        "P4": 12,
-        "P5": 10,
-        "P6": 8,
-        "P7": 6,
-        "P8": 4,
-        "P9": 2,
-        "P10": 1
-    }
-
-    def __init__(self, racesInSeason):
+    def __init__(self, racesInSeason, pointsDistributionSystem):
         self.racesInSeason = racesInSeason
+        self.PointsDistributionSystem = pointsDistributionSystem
 
-    def GivePoints(driver=dict, position=str):
+    def GivePoints(driver, position=str):
         return driver + position # returns value of the amount of points driver has. 
+    
+    def checkIfInPoints(driver, position):
+        """
+        returns true or false if the driver is in top ten or not
 
+        psuedo:
+
+        if driver postion >= 10:
+            return True
+
+        else:
+            return False
+        """
+        pass
+
+
+# test
+seasonSimulation.GivePoints(pointsThisSeason["pointsMV"], )
+
+
+class driverInRace:
+    def __init__(self, position) -> None:
+        self.position = position
